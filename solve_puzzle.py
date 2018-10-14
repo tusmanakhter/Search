268d5ece4365f92c10a73be6e0cb2fba9a381831
tusmanakhter <tusman96@hotmail.com>
2018-10-14 17:46:18 -0400

    Added exec time

--- solve_puzzle.py
+++ solve_puzzle.py
@@ -1,14 +1,16 @@
 import argparse
 from Node import Node
 from Search import Search
+from time import perf_counter
 
 
-def print_results(title, search, file):
+def print_results(title, search, file, start_time, end_time):
     print("\n" + title + ":\n")
     print("Total = " + str(len(search.open_list)+len(search.closed_list)) +
           ", Open list = " + str(len(search.open_list)) +
           ", Closed list = " + str(len(search.closed_list)) +
-          ", Path length = " + str(len(search.path)) + "\n")
+          ", Path length = " + str(len(search.path)) +
+          ", Exec time = " + str(end_time - start_time) + "s \n")
     print(search.trace_to_string())
     with open(file, "w") as file:
         file.write(search.trace_to_string())
@@ -40,37 +42,49 @@ if len(set(initial_puzzle)) != len(initial_puzzle):
     parser.error("Please do not have any duplicate values in puzzle.")
 
 if args.b:
+    start_breadth_first = perf_counter()
     initial_node = Node(initial_puzzle, args.columns, None, "0", 0)
     breadth_first = Search(initial_node)
     breadth_first.search("breadth_first")
+    end_breadth_first = perf_counter()
 
+start_depth_first = perf_counter()
 initial_node = Node(initial_puzzle, args.columns, None, "0", 0)
 depth_first = Search(initial_node)
 depth_first.search("depth_first", args.max_depth, args.i)
+end_depth_first = perf_counter()
 
+start_best_first_linear = perf_counter()
 initial_node = Node(initial_puzzle, args.columns, None, "0", 0, "linear_distance")
 best_first_linear = Search(initial_node)
 best_first_linear.search("best_first")
+end_best_first_linear = perf_counter()
 
+start_best_first_wrong = perf_counter()
 initial_node = Node(initial_puzzle, args.columns, None, "0", 0, "wrong_row_column")
 best_first_wrong = Search(initial_node)
 best_first_wrong.search("best_first")
+end_best_first_wrong = perf_counter()
 
+start_a_linear = perf_counter()
 initial_node = Node(initial_puzzle, args.columns, None, "0", 0, "linear_distance")
 a_linear = Search(initial_node)
 a_linear.search("a")
+end_a_linear = perf_counter()
 
+start_a_wrong = perf_counter()
 initial_node = Node(initial_puzzle, args.columns, None, "0", 0, "wrong_row_column")
 a_wrong = Search(initial_node)
 a_wrong.search("a")
+end_a_wrong = perf_counter()
 
 print("\n----- Solutions -----\n")
 if args.b:
-    print_results("Breadth First Search", breadth_first, "puzzleBreadth.txt")
-print_results("Depth First Search", depth_first, "puzzleDFS.txt")
-print_results("Best First Search (Linear Distance)", best_first_linear, "puzzleBFS-h1.txt")
-print_results("Best First Search (Wrong Row Column)", best_first_wrong, "puzzleBFS-h2.txt")
-print_results("A* Search (Linear Distance)", a_linear, "puzzleAs-h1.txt")
-print_results("A* Search (Wrong Row Column)", a_wrong, "puzzleAs-h2.txt")
+    print_results("Breadth First Search", breadth_first, "puzzleBreadth.txt", start_breadth_first, end_breadth_first)
+print_results("Depth First Search", depth_first, "puzzleDFS.txt", start_depth_first, end_depth_first)
+print_results("Best First Search (Linear Distance)", best_first_linear, "puzzleBFS-h1.txt", start_best_first_linear, end_best_first_linear)
+print_results("Best First Search (Wrong Row Column)", best_first_wrong, "puzzleBFS-h2.txt", start_best_first_wrong, end_best_first_wrong)
+print_results("A* Search (Linear Distance)", a_linear, "puzzleAs-h1.txt", start_a_linear, end_a_linear)
+print_results("A* Search (Wrong Row Column)", a_wrong, "puzzleAs-h2.txt", start_a_wrong, end_a_wrong)
 
 
